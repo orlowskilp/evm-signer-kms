7afc45af57ada27ee553449709a2a74bab2730ad
orlowskilp <lukasz@orlowski.io>
2025-05-11 16:47:05 +0800

    chore: Added comments to devcontainer setup commands

--- .devcontainer/commands/post-create.sh
+++ .devcontainer/commands/post-create.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+# Marks the workspace root directory as a safe directory for git
 git config --global --add safe.directory $(pwd)
--- .devcontainer/commands/post-start.sh
+++ .devcontainer/commands/post-start.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+# Easy way to ensure devcontainer can talk to Docker daemon on the host after binding
 DOCKER_SOCK=/var/run/docker.sock
-
 sudo chmod 777 ${DOCKER_SOCK}
 
+# Install Rust toolchains from `rust-toolchain.toml` file
 rustup toolchain install
