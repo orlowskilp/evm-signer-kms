be323c0ba2af2862534eb442033aadaf1ab1b973
orlowskilp <lukasz@orlowski.io>
2025-07-21 11:13:27 +0800

    docs: Updated examples in docs

--- src/lib.rs
+++ src/lib.rs
@@ -16,85 +16,68 @@
 //! [`EIP-1559`](https://eips.ethereum.org/EIPS/eip-1559) transaction:
 //!
 //! ```rust
-//! use evm_signer_kms::{
-//!     key::aws_kms::AwsKmsKey,
-//!     account::EvmAccount,
-//!     transaction::{
-//!         address::AccountAddress,
-//!         free_market_transaction::FreeMarketTransaction,
-//!         SignedTransaction, Transaction
-//!     }};
-//! use std::{env, io::{Error, ErrorKind, Result}};
+//!  use anyhow::{Result, bail};
+//!  use evm_signer_kms::{
+//!      account::EvmAccount, key::aws_kms::AwsKmsKey,
+//!      transaction::free_market_transaction::FreeMarketTransaction,
+//!  };
+//!  use std::env;
 //!
-//! // Name of the environment variable that contains the KMS key ID
-//! const KMS_KEY_ID_VAR_NAME: &str = "KMS_KEY_ID";
-//! // Example EIP-1559 transaction JSON
-//! const FREE_MARKET_TX_JSON: &str = r#"
-//! {
-//!     "gasLimit": 21000,
-//!     "maxFeePerGas": 100000000000,
-//!     "maxPriorityFeePerGas": 3000000000,
-//!     "chainId": 11155111,
-//!     "nonce": 0,
-//!     "to": "0xD1220A0cf47c7B9Be7A2E6BA89F429762e7b9aDb",
-//!     "value": 10000000000000000,
-//!     "data": "",
-//!     "accessList": []
-//! }
-//! "#;
+//!  // Name of the environment variable that contains the KMS key ID
+//!  const KMS_KEY_ID_VAR_NAME: &str = "KMS_KEY_ID";
+//!  // Example EIP-1559 transaction JSON
+//!  const FREE_MARKET_TX_JSON: &str = r#"
+//!  {
+//!      "gasLimit": 21000,
+//!      "maxFeePerGas": 100000000000,
+//!      "maxPriorityFeePerGas": 3000000000,
+//!      "chainId": 11155111,
+//!      "nonce": 0,
+//!      "to": "0xD1220A0cf47c7B9Be7A2E6BA89F429762e7b9aDb",
+//!      "value": 10000000000000000,
+//!      "data": "",
+//!      "accessList": []
+//!  }
+//!  "#;
 //!
-//! // Communication with AWS endpoint is asynchronous, so we need to use async main function
-//! #[tokio::main]
-//! async fn main() -> Result<()> {
-//!     // Get KMS key ID from environment variable
-//!     let kms_key_id = env::var(KMS_KEY_ID_VAR_NAME).map_err(|_| {
-//!         Error::new(ErrorKind::NotFound, format!(
-//!             "Environment variable {} not set",
-//!             KMS_KEY_ID_VAR_NAME,
-//!         ))
-//!     })?;
+//!  // Communication with AWS endpoint is asynchronous, so we need to use async main function
+//!  #[tokio::main]
+//!  async fn main() -> Result<()> {
+//!      // Get KMS key ID from environment variable
+//!      let kms_key_id = env::var(KMS_KEY_ID_VAR_NAME).or_else(|_| {
+//!          bail!("Not set: {KMS_KEY_ID_VAR_NAME}");
+//!      })?;
 //!
-//!     // Create a new KMS key
-//!     let kms_key = &AwsKmsKey::new(&kms_key_id).await;
-//!     // Create a new EVM account
-//!     let evm_account = EvmAccount::new(kms_key)
-//!         .await
-//!         .map_err(|error| Error::new(ErrorKind::PermissionDenied, format!(
-//!             "Failed to create EVM account: {}",
-//!             error,
-//!         ))
-//!     )?;
+//!      // Create a new KMS key
+//!      let kms_key = &AwsKmsKey::new(&kms_key_id).await;
+//!      // Create a new EVM account
+//!      let evm_account = EvmAccount::new(kms_key).await.or_else(|err| {
+//!          bail!("Create EVM account: {err}");
+//!      })?;
 //!
-//!     // Create a new unsigned EIP-1559 transaction
-//!     let unigned_tx = serde_json::from_str::<FreeMarketTransaction>(FREE_MARKET_TX_JSON)
-//!         .map_err(|error| Error::new(ErrorKind::InvalidInput, format!(
-//!            "Failed to parse transaction JSON: {}",
-//!            error,
-//!        ))
-//!     )?;
+//!      // Create a new unsigned EIP-1559 transaction
+//!      let unigned_tx =
+//!          serde_json::from_str::<FreeMarketTransaction>(FREE_MARKET_TX_JSON).or_else(|err| {
+//!              bail!("Parse transaction JSON: {err}");
+//!          })?;
 //!
-//!     // Sign the transaction using EVM account
-//!     let signed_tx = evm_account
-//!         .sign_transaction(unigned_tx)
-//!         .await
-//!         .map_err(|error| Error::new(ErrorKind::PermissionDenied, format!(
-//!             "Failed to sign transaction: {}",
-//!             error,
-//!         ))
-//!     )?;
+//!      // Sign the transaction using EVM account
+//!      let signed_tx = evm_account
+//!          .sign_transaction(unigned_tx)
+//!          .await
+//!          .or_else(|err| {
+//!              bail!("Sign transaction: {err}");
+//!          })?;
 //!
-//!     // Use the provided string serialization to get the signed transaction encoding.
-//!     let signed_tx_encoding = serde_plain::to_string(&signed_tx)
-//!         .map_err(|error| Error::new(ErrorKind::InvalidData, format!(
-//!            "Failed to serialize signed transaction: {}",
-//!            error,
-//!       ))
-//!     )?;
+//!      // Use the provided string serialization to get the signed transaction encoding.
+//!      let signed_tx_encoding = serde_plain::to_string(&signed_tx).or_else(|err| {
+//!          bail!("Serialize signed transaction: {err}");
+//!      })?;
 //!
-//!     println!("Signed transaction encoding: {}", signed_tx_encoding);
+//!      println!("Encoded signed type-2 transaction: {signed_tx_encoding}");
 //!
-//!     Ok(())
-//! }
+//!      Ok(())
+//!  }
 //! ```
 //!
 //! ## Access list transaction (i.e. type 1 transaction)
